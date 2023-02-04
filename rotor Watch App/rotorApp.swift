6b41f3877d3569ef5b8ad5d1b72a1a5b7de9f628
PetarSimonovic <petar@simonovic.com>
2023-02-04 13:52:12 +0000

    Initial Commit

---
+++ rotor Watch App/rotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotorApp.swift
+//  rotor Watch App
+//
+//  Created by Petar Simonovic on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct rotor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rotor/rotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotorApp.swift
+//  rotor
+//
+//  Created by Petar Simonovic on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct rotorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
